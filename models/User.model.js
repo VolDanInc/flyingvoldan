e9aed89e4d3bf7413673be57f5e25978afe8e231
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-27 11:29:32 +0300

    changed user model and auth route

--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    isAdmin: {
+      type: Boolean,
+      
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
